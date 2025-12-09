46ee177e7202c80eaa2635347b2d3aaedc89fdd3
Rex Sun <3117553927@qq.com>
2025-12-09 12:54:28 +0800

    1

--- src/main/java/com/qpwflshclub/formal_club/FormalClubApplication.java
+++ src/main/java/com/qpwflshclub/formal_club/FormalClubApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FormalCLubApplication {
+public class FormalClubApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FormalCLubApplication.class, args);
+        SpringApplication.run(FormalClubApplication.class, args);
     }
 
 }
